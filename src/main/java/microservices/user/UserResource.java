b15e4b878f91e0373ccb761c7911ce5b2a5d5819
panu <panu31@yahoo.co.uk>
2018-06-20 20:05:26 -0700

    Added logging.

--- src/main/java/microservices/user/UserResource.java
+++ src/main/java/microservices/user/UserResource.java
@@ -1,5 +1,7 @@
 package microservices.user;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -7,12 +9,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 public class UserResource {
+
+    private static Logger logger = LogManager.getLogger(UserResource.class);
 
     @Autowired
     private UserReposity userReposity;
@@ -28,23 +33,24 @@ public class UserResource {
         return one.get();
     }
 
-    @GetMapping("/users/{id}/orders/")
-    public List<Purchase> getUserOrder(@PathVariable int id) {
+    @GetMapping("/users/{id}/orders/cid/{cid}")
+    public List<Purchase> getUserOrder(@PathVariable int id, @PathVariable int cid) {
 
+        logger.info("CALL\t{}\tINIT\tUsers\t/users/{}/orders", cid, id);
 
-        final String productUri = "http://localhost:8092/products/";
-        final String orderUri = "http://localhost:8093/orders/";
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        ResponseEntity<List<Purchase>> rateResponse =
-                restTemplate.exchange(orderUri,
-                        HttpMethod.GET, null, new ParameterizedTypeReference<List<Purchase>>() {
-                        });
-        List<Purchase> allOrders = rateResponse.getBody();
+        final String productUri = "http://localhost:8092/products";
+        final String orderUri = "http://localhost:8093/orders";
 
         List<Purchase> result = new ArrayList<>();
+        RestTemplate restTemplate = new RestTemplate();
 
+        try {
+            logger.info("CALL\t{}\tUsers\tOrders\t{}", cid, orderUri);
+            ResponseEntity<List<Purchase>> rateResponse =
+                    restTemplate.exchange(orderUri + "/cid/" + cid + "/caller/Users",
+                            HttpMethod.GET, null, new ParameterizedTypeReference<List<Purchase>>() {
+                            });
+        List<Purchase> allOrders = rateResponse.getBody();
         System.out.println("id: "+id);
 
         for (Purchase order : allOrders) {
@@ -52,12 +58,17 @@ public class UserResource {
             if (order.getUserId() == id) {
                 result.add(order);
 
-                final String prodIdUri = productUri + order.getProductId();
-                Product prod = restTemplate.getForObject(prodIdUri, Product.class);
+                final String prodIdUri = productUri + "/" + order.getProductId();
+                logger.info("CALL\t{}\tUsers\tProducts\t{}", cid, prodIdUri);
+                Product prod = restTemplate.getForObject(prodIdUri + "/cid/" + cid + "/caller/Users", Product.class);
                 System.out.println("Prod name : " + prod.getName());
                 order.setProductName(prod.getName());
             }
         }
+        }catch(Exception e){
+            logger.info("FAIL\t{}\t-----\t-----\t{}", cid, e.getMessage());
+        }
+
         return result;
     }
 
